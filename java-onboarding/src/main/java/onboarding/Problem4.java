a69233c410f9f3be8f8112cb239c40585a7d6b27
hyeonwook98 <hyeonwook98@naver.com>
2022-11-17 15:30:24 +0900

    feat: 'Problem 4' 완료

--- java-onboarding/src/main/java/onboarding/Problem4.java
+++ java-onboarding/src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String result = "";
+
+        char[] arrWord = word.toCharArray();
+        for (char alphabet : arrWord) {
+            result += changeWord(alphabet);
+        }
+
+        return result;
+    }
+
+    public static char changeWord(char alphabet) {
+        if('a' <= alphabet && alphabet <= 'z') {
+            return (char) ('a' + 'z' - alphabet);
+        }
+        if ('A' <= alphabet && alphabet <= 'Z') {
+            return (char) ('A' + 'Z' - alphabet);
+        }
+        return alphabet;
     }
 }
