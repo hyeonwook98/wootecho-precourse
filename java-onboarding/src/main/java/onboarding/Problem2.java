e11ae9eebc76157e8251130baa6da2c043b60959
hyeonwook98 <hyeonwook98@naver.com>
2022-11-18 17:20:50 +0900

    refactor(Problem2): 정규표현식 적용 및 코드 리팩토링

--- java-onboarding/src/main/java/onboarding/Problem2.java
+++ java-onboarding/src/main/java/onboarding/Problem2.java
@@ -4,56 +4,40 @@ import java.util.ArrayList;
 
 public class Problem2 {
 
-    static private String result = "\"\"";
+    static private String BLANK = "";
+    static private final String FIND_REGEX = "{2,}";
+    static private String result = "";
+
     public static String solution(String cryptogram) {
 
-        String str = cryptogram;
+        boolean flag = true;
+        String beforeCryptogram = cryptogram;
+        String afterCryptogram = "";
 
-        ArrayList<Character> list = new ArrayList<Character>();
+        while (flag) {
+            afterCryptogram = decrypt(beforeCryptogram);
 
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            list.add(c);
+            if (isDifferent(beforeCryptogram, afterCryptogram)) {
+                beforeCryptogram = afterCryptogram;
+                continue;
+            }
+            result = afterCryptogram;
+            flag = false;
         }
-
-        if (list.size() == 0) {
-            return result;
-        }
-
-        ArrayList<Character> findStr = strRemove(list);
-        return getResult(findStr);
-
+        return result;
     }
 
-    public static ArrayList<Character> strRemove(ArrayList<Character> list) {
-        ArrayList<Character> strList = list;
-        int index = 0;
-
-        while (strList.size() != 0) {
-            if (index == -1 || (index == strList.size() - 1)) {
-                break;
-            }
-            if (strList.get(index) == strList.get(index+1)) {
-                strList.remove(index+1);
-                strList.remove(index);
-                if (index != 0) {
-                    index -= 1;
-                }
-            }
-            else if (strList.get(index) != strList.get(index+1)) {
-                index += 1;
-            }
-        }
-
-        return strList;
+    public static Boolean isDifferent(String beforeCryptogram, String cryptogram) {
+        return beforeCryptogram != cryptogram;
     }
 
-    public static String getResult(ArrayList<Character> list) {
-
-        String str = "";
-        for (Character c : list) {
-            str += c;
+    public static String decrypt(String cryptogram) {
+        for (int alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+            String regex = (char)alphabet + FIND_REGEX;
+            cryptogram = cryptogram.replaceAll(regex, BLANK);
         }
-        return str;
+        return cryptogram;
     }
+
+
 }
